2539e6faee4a6974f3e32bdda76d29ba2dc74bc6
GerardoA23 <cg.23alvarado@gmail.com>
2025-09-19 21:33:06 -0500

    Clase19/09/2025

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './ejercicios/Ejercicio01-tipos-datos';
+import './ejercicios/Ejercicio02-objetos-arreglos';
+import './ejercicios/Ejercicio03-funciones';
+import './ejercicios/Ejercicio_1';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+app.innerHTML = `Hola mundo`;
+console.log('Holaaaa');
